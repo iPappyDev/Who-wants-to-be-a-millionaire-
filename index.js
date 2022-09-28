6ba04791d1a48855821430fd9c35c1dfec4dd056
iPappyDev <pappythorium@gmail.com>
2022-09-28 11:24:54 +0100

    added more questions

--- index.js
+++ index.js
@@ -1,29 +1,104 @@
 let questionBank = [
     {
-        question : 'Entomology is the science that studies',
-        option : ['Behavior of human beings','Insects','The origin and history ','The formation of rocks'],
-        answer : 'Insects'
+        question : 'What has a neck but no head?',
+        option : ['Pen','AirPod','A bottle','Pebbles'],
+        answer : 'A bottle'
     },
     {
-        question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
-        option : ['Asia','Africa','Europe','Australia'],
-        answer : 'Africa'
+        question : 'What is the coldest country in the world?',
+        option : ['Vietnam','Russia','Chili','Canada'],
+        answer : 'Chili'
     },
     {
-        question : 'For which of the following disciplines is Nobel Prize awarded?',
-        option : ['Physics and Chemistry','Physiology or Medicine','Literature and Economics','All of the above'],
-        answer : 'All of the above'
+        question : 'What bird can lift the most weight?',
+        option : ['Eagle','A crane','Hawk','All of the above'],
+        answer : 'A crane'
     },
     {
-        question : 'Exposure to sunlight helps a person improve his health because',
-        option : ['kills bacteria in the body','resistance power increases','skin cells stimulated tan','convert UV rays to Vit D'],
-        answer : 'convert UV rays to Vit D'
-    }
-    // {
-    //     question : 'Which player scored the fastest hat-trick in the Premier League?',
-    //     option : ['Sadio Mane', 'Michael Carrick', 'Harry Kane', 'Scott Mc'],
-    //     answer : 'Sadio Mane'
-    // }
+        question : 'Why did the woman run around her bed at night?',
+        option : ['kills bacteria in the body','To catch up on her sleep','To catch warm','Horny'],
+        answer : 'To catch up on her sleep'
+    },
+    {
+        question : 'Which player scored the fastest hat-trick in the Premier League?',
+        option : ['Harry Kane', 'Sadio Mane', 'Michael Carrick', 'Scott Mc'],
+        answer : 'Sadio Mane'
+    },
+    {
+        question : 'If the vice president were to die, who is supposed to be president?',
+        option : ['President adviser', 'Senate president', 'The President', 'None of the above'],
+        answer : 'The President'
+    },
+    {
+        question: 'What goes up as soon as the rain comes down?',
+        option: ['Clutch','An umbrella','Cloud','Cap'],
+        answer: 'An umbrella'
+    },
+    {
+        question: 'The more you take, the more you leave behind. What am I?',
+        option: ['Legprints','Handprints','Footprints','Fingerprints'],
+        answer: 'Footprints'
+    },
+    {
+        question: 'How many animals did Moses take into the ark?',
+        option: ['4','7','None','10'],
+        answer: 'None'
+    },
+    {
+        question: 'What has three feet but can’t walk?',
+        option: ['Mopping Stick','Tree Root','A yardstick','None'],
+        answer: 'A yardstick'
+    },
+    {
+        question: 'If two’s company and three’s a crowd, what do four and five make?',
+        option: ['Family','Nine','House','Market'],
+        answer: 'Nine'
+    },
+    {
+        question: 'What travels the world while stuck in one spot?',
+        option: ['All of the above','Letter','A stamp','Signature'],
+        answer: 'A stamp'
+    },
+    {
+        question: 'Which room has no walls?',
+        option: ['Root','A Mushroom','Stem','All of the above'],
+        answer: 'A Mushroom'
+    },
+    {
+        question: 'What has four eyes but can’t see?',
+        option: ['Owl','Dart','Mississippi','Plate'],
+        answer: 'Mississippi'
+    },
+    {
+        question: 'Where is an ocean with no water?',
+        option: ['Road','Control','A map','All of the above'],
+        answer: 'A map'
+    },
+    {
+        question: 'How can a door be not a door?',
+        option: ['Locked','Sealed','Jammed','Closed'],
+        answer: 'When it’s a jar'
+    },
+    {
+        question: 'I have one head, one foot, and four legs. What am I?',
+        option: ['CHair','bed','Table','All of the above'],
+        answer: ''
+    },
+    {
+        question: 'What has one eye but can’t see anything at all?',
+        option: ['Broom','Thread','A needle','Mice'],
+        answer: 'A needle'
+    },
+    {
+        question: 'What kind of tree can you carry in your hand?',
+        option: ['Leave','A palm','Paper','None of the above'],
+        answer: 'A palm'
+    },
+    {
+        question: 'How many times can you subtract 10 from 100?',
+        option: ['Ten times','Once','Twice','Five times'],
+        answer: 'Once'
+    },
 ]
 let amount = document.getElementById('amountEarned')
 let question = document.getElementById('question');
